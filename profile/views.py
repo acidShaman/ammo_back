414d9018bc88986b43e4aa9cea9939feab581930
acid_shaman <andrewmotruk80@gmail.com>
2020-08-26 16:09:14 +0300

    Created models, added login and register views

--- profile/views.py
+++ profile/views.py
@@ -1,10 +1,7 @@
-from rest_framework.mixins import ListModelMixin
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.utils import json
 from rest_framework.views import APIView
-
 from .serializers import ProfileSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from profile.models import ProfileModel
@@ -24,12 +21,12 @@ class CreateProfileView(APIView):
 
     @classmethod
     def post(cls, request: Request, *args, **kwargs):
+        print(request.data.keys())
+        if request.data.keys() not in ['username', 'first_name', 'last_name', 'phone', 'birthday', 'password']:
+            return Response({'message': 'Not enough fields to create new user!'})
         user = User(username=request.data['username'], first_name=request.data['first_name'], last_name=request.data['last_name'])
         profile = ProfileModel(user=user, phone=request.data['phone'], birthday=request.data['birthday'])
         profile.user.set_password(request.data['password'])
-        # user.profile.phone = request.data['phone']
-        # user.profile.birthday = request.data['birthday']
-        # user.set_password(request.data['password'])
         user.save()
         profile.save()
         return Response({'message': 'Done'})
